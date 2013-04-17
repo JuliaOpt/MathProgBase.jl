66a50f741f5b7f5679b9b2e443fe4648ecf9938b
Miles Lubin <miles.lubin@gmail.com>
2013-04-17 14:04:48 -0400

    use Test module for tests

--- test/linprog.jl
+++ test/linprog.jl
@@ -1,24 +1,32 @@
+using Base.Test
+using MathProgBase
+
 # min -x
 # s.t. 2x + y <= 1.5
 # x,y >= 0
 # solution is (0.75,0) with objval -0.75
 
 sol = linprog([-1,0],[2 1],'<',1.5)
-@assert sol.status == :Optimal && norm(sol.objval+0.75) < 1e-7 && norm(sol.sol - [0.75,0.0]) < 1e-7
-linprog([-1,0],sparse([2 1]),'<',1.5)
-@assert sol.status == :Optimal && norm(sol.objval+0.75) < 1e-7 && norm(sol.sol - [0.75,0.0]) < 1e-7
+@test sol.status == :Optimal
+@test_approx_eq sol.objval -0.75
+@test_approx_eq norm(sol.sol - [0.75,0.0]) 0 
+
+sol = linprog([-1,0],sparse([2 1]),'<',1.5)
+@test sol.status == :Optimal
+@test_approx_eq sol.objval -0.75
+@test_approx_eq norm(sol.sol - [0.75,0.0]) 0 
 
 # test infeasible problem:
 # min x
 # s.t. 2x+y <= -1
 # x,y >= 0
 sol = linprog([1,0],[2 1],'<',-1)
-@assert sol.status == :Infeasible
+@test sol.status == :Infeasible
 
 # test unbounded problem:
 # min -x-y
 # s.t. -x+2y <= 0
 # x,y >= 0
 sol = linprog([-1,-1],[-1 2],'<',[0])
-@assert sol.status == :Unbounded
+@test sol.status == :Unbounded
 
