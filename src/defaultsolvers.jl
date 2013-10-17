b8c9459bc4d683ef9d1b5222ecbc79ef97a64b2c
Miles Lubin <miles.lubin@gmail.com>
2013-10-17 15:50:01 -0400

    macros for choosing default solvers

---
+++ src/defaultsolvers.jl
@@ -0,0 +1,36 @@
+
+# macros to generate code to set default solver
+
+macro setdefaultLPsolver()
+    solvers = [(:Clp,:ClpSolver)]
+    for (pkgname, solvername) in solvers
+        if Pkg.installed(string(pkgname)) != nothing
+            importexpr = Expr(:import,pkgname)
+            return esc(quote 
+                $importexpr
+                const defaultLPsolver = ($(pkgname).$(solvername))()
+            end)
+        end
+    end
+    pkgnames = [pkgname for (pkgname, solvername) in solvers]
+    return esc(quote
+        const defaultLPSolver = MissingSolver("LP",$pkgnames)
+    end)
+end
+
+macro setdefaultMIPsolver()
+    solvers = [(:Cbc,:CbcSolver)]
+    for (pkgname, solvername) in solvers
+        if Pkg.installed(string(pkgname)) != nothing
+            importexpr = Expr(:import,pkgname)
+            return esc(quote 
+                $importexpr
+                const defaultMIPsolver = ($(pkgname).$(solvername))()
+            end)
+        end
+    end
+    pkgnames = [pkgname for (pkgname, solvername) in solvers]
+    return esc(quote
+        const defaultLPSolver = MissingSolver("MIP",$pkgnames)
+    end)
+end
