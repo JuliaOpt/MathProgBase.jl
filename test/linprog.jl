3d898a721e36c3626e99647be23a39351d043937
Carlo Baldassi <carlobaldassi@gmail.com>
2013-10-21 14:36:46 +0200

    Fix tests (default solver capitalization)

--- test/linprog.jl
+++ test/linprog.jl
@@ -1,7 +1,7 @@
 using Base.Test
 using MathProgBase
 
-function linprogtest(solver=MathProgBase.defaultLPsolver)
+function linprogtest(solver=MathProgBase.defaultLPSolver)
     println("Testing linprog with solver ", string(typeof(solver)))
     # min -x
     # s.t. 2x + y <= 1.5
