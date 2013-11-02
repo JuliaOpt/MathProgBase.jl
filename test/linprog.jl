76c2b7d943d7ef1532537a60ced5d7260bc0a57c
Miles Lubin <miles.lubin@gmail.com>
2013-11-02 14:07:52 -0400

    accept tolerances for linprog test. closes #12

--- test/linprog.jl
+++ test/linprog.jl
@@ -1,7 +1,7 @@
 using Base.Test
 using MathProgBase
 
-function linprogtest(solver=MathProgBase.defaultLPsolver)
+function linprogtest(solver=MathProgBase.defaultLPsolver; objtol = 1e-7, primaltol = 1e-6)
     println("Testing linprog with solver ", string(typeof(solver)))
     # min -x
     # s.t. 2x + y <= 1.5
@@ -10,13 +10,13 @@ function linprogtest(solver=MathProgBase.defaultLPsolver)
 
     sol = linprog([-1,0],[2 1],'<',1.5,solver)
     @test sol.status == :Optimal
-    @test_approx_eq sol.objval -0.75
-    @test_approx_eq norm(sol.sol - [0.75,0.0]) 0 
+    @test_approx_eq_eps sol.objval -0.75 objtol
+    @test_approx_eq_eps norm(sol.sol - [0.75,0.0]) 0 primaltol
 
     sol = linprog([-1,0],sparse([2 1]),'<',1.5,solver)
     @test sol.status == :Optimal
-    @test_approx_eq sol.objval -0.75
-    @test_approx_eq norm(sol.sol - [0.75,0.0]) 0 
+    @test_approx_eq_eps sol.objval -0.75 objtol
+    @test_approx_eq_eps norm(sol.sol - [0.75,0.0]) 0 primaltol
 
     # test infeasible problem:
     # min x
