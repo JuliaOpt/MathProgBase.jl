120de9581bf3f7da83a64f44d361a564f2e40b71
Miles Lubin <miles.lubin@gmail.com>
2014-01-21 10:42:05 -0500

    loosen tolerances. closes #18

--- test/mixintprog.jl
+++ test/mixintprog.jl
@@ -7,8 +7,8 @@ function mixintprogtest(solver=MathProgBase.defaultMIPsolver)
     # integer knapsack problem
     sol = mixintprog(-[5,3,2,7,4],Float64[2 8 4 2 5],'<',10,'I',0,1,solver)
     @test sol.status == :Optimal
-    @test_approx_eq sol.objval -16.0
-    @test_approx_eq norm(sol.sol[1:5] - [1.0, 0.0, 0.0, 1.0, 1.0]) 0.0
+    @test_approx_eq_eps sol.objval -16.0 1e-6
+    @test_approx_eq_eps norm(sol.sol - [1.0, 0.0, 0.0, 1.0, 1.0]) 0.0 1e-4
 
     println("Done")
 end
