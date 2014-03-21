6b978b3e6fbfa33e6705661062af692035534ba7
Joey Huchette <joehuchette@gmail.com>
2014-03-21 01:47:10 -0400

    good to go

--- src/MathProgSolverSDPInterface.jl
+++ src/MathProgSolverSDPInterface.jl
@@ -1,8 +1,14 @@
+export addSDPvar!,
+       addSDPmatrix!,
+       addSDPconstr!,
+       setSDPobj!,
+       getSDPvarsolution
+
 addSDPvar!(m::AbstractMathProgModel, dim) = error("Not implemented")
 addSDPmatrix!(m::AbstractMathProgModel, mat) = error("Not implemented")
 
-addSDPconstr!(m::AbstractMathProgModel, varidx, coefidx, rowlb, rowub) = error("Not implemented")
+addSDPconstr!(m::AbstractMathProgModel, matvaridx, matcoefidx, scalidx, scalcoef, lb, ub) = error("Not implemented")
 
-setSDPobj!(m::AbstractMathProgModel, coefidx) = error("Not implemented")
+setSDPobj!(m::AbstractMathProgModel, matvaridx, matcoefidx) = error("Not implemented")
 
 getSDPvarsolution(m::AbstractMathProgModel, idx) = error("Not implemented")
