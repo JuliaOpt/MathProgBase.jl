805c35e3a7fe58e780691e3c31f389b4f449e7da
Miles Lubin <miles.lubin@gmail.com>
2014-05-21 17:30:46 -0400

    refix docs

--- doc/conf.py
+++ doc/conf.py
@@ -33,7 +33,7 @@ sys.path.insert(0, os.path.abspath('sphinx'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.mathjax', 'sphinx.ext.todo', 'sphinx.ext.autodoc']
+extensions = ['sphinx.ext.mathjax', 'juliadoc.julia', 'juliadoc.jlhelp']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
