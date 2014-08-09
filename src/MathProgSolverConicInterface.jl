38496c9da765fe43bd5d3b5b89404dbd4393a6c0
Madeleine Udell <madeleine.udell@gmail.com>
2014-08-08 17:09:18 -0700

    added loadineqconicproblem interface

--- src/MathProgSolverConicInterface.jl
+++ src/MathProgSolverConicInterface.jl
@@ -1,4 +1,4 @@
-funcs = [:loadconicproblem!, :getconicdual]
+funcs = [:loadconicproblem!, :loadineqconicproblem!, :getconicdual]
 
 for func in funcs
     @eval $(func)() = throw(MethodError($(func),()))
