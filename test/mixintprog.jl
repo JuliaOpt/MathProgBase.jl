e707e99c7b47e8ccf748a66d15c7be2c3ee8041d
Joey Huchette <joehuchette@gmail.com>
2014-08-29 21:00:05 -0400

    fix broken test.

--- test/mixintprog.jl
+++ test/mixintprog.jl
@@ -5,7 +5,7 @@ function mixintprogtest(solver=MathProgBase.defaultMIPsolver)
     println("Testing mixintprog with solver ", string(typeof(solver)))
 
     # integer knapsack problem
-    sol = mixintprog(-[5,3,2,7,4],Float64[2 8 4 2 5],'<',10,'I',0,1,solver)
+    sol = mixintprog(-[5,3,2,7,4],Float64[2 8 4 2 5],'<',10,:Int,0,1,solver)
     @test sol.status == :Optimal
     @test_approx_eq_eps sol.objval -16.0 1e-6
     @test_approx_eq_eps norm(sol.sol - [1.0, 0.0, 0.0, 1.0, 1.0]) 0.0 1e-4
