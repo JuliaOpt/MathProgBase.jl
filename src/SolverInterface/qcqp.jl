c4b6d47a6d0dc1a51ad6cab2119964496a72bffd
Miles Lubin <miles.lubin@gmail.com>
2014-09-04 20:27:12 -0400

    better error message for solvers which don't support quadratic objectives

--- src/SolverInterface/qcqp.jl
+++ src/SolverInterface/qcqp.jl
@@ -12,34 +12,42 @@ function setquadobjterms!(m::AbstractMathProgModel, rowidx, colidx, quadval)
             nquadval[i] *= 2
         end
     end
-    setquadobj!(m, rowidx, colidx, nquadval)
+    if applicable(setquadobj!, m, rowidx, colidx, nquadval)
+        setquadobj!(m, rowidx, colidx, nquadval)
+    else
+        error("Solver does not support quadratic objectives")
+    end
 end
 
 setquadobj!(m::AbstractMathProgModel,Q::Matrix) = setquadobj!(m,sparse(float(Q)))
 function setquadobj!(m::AbstractMathProgModel,Q::SparseMatrixCSC{Float64})
-  if issym(Q) || istriu(Q)
-    nnz_q = nnz(Q)
-    qr = Array(Cint, nnz_q)
-    qc = Array(Cint, nnz_q)
-    qv = Array(Cdouble, nnz_q)
-    k = 0
-    colptr::Vector{Int} = Q.colptr
-    nzval::Vector{Float64} = Q.nzval
+    if issym(Q) || istriu(Q)
+        nnz_q = nnz(Q)
+        qr = Array(Cint, nnz_q)
+        qc = Array(Cint, nnz_q)
+        qv = Array(Cdouble, nnz_q)
+        k = 0
+        colptr::Vector{Int} = Q.colptr
+        nzval::Vector{Float64} = Q.nzval
 
-    for i = 1:numvar(m)
-      qi::Cint = convert(Cint, i)
-      for j = colptr[i]:(colptr[i+1]-1)
-        qj = convert(Cint, Q.rowval[j])
-        if qi <= qj
-          k += 1
-          qr[k] = qi
-          qc[k] = qj
-          qv[k] = nzval[j]
+        for i = 1:numvar(m)
+            qi::Cint = convert(Cint, i)
+            for j = colptr[i]:(colptr[i+1]-1)
+                qj = convert(Cint, Q.rowval[j])
+                if qi <= qj
+                    k += 1
+                    qr[k] = qi
+                    qc[k] = qj
+                    qv[k] = nzval[j]
+                end
+            end
         end
-      end
+        if applicable(setquadobj!, m, qr, qc, qv)
+            setquadobj!(m,qr[1:k],qc[1:k],qv[1:k])
+        else
+            error("Solver does not support quadratic objectives")
+        end
+    else
+        error("Quadratic cost coefficient matrix Q is not symmetric or upper triangular")
     end
-    setquadobj!(m,qr[1:k],qc[1:k],qv[1:k])
-  else
-    error("Quadratic cost coefficient matrix Q is not symmetric or upper triangular")
-  end
 end
