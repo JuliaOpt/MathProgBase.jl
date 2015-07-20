64e2e687f32461120ca8e3abad7bdb6cf3f497a0
Iain Dunning <iaindunning@gmail.com>
2015-07-20 16:52:49 -0400

    Remove requires

--- src/MathProgBase.jl
+++ src/MathProgBase.jl
@@ -1,15 +1,15 @@
 module MathProgBase
 
-require(joinpath(dirname(@__FILE__),"SolverInterface","SolverInterface.jl"))
-using SolverInterface
+include(joinpath(dirname(@__FILE__),"SolverInterface","SolverInterface.jl"))
+using .SolverInterface
 
 # deprecated name
 const MathProgSolverInterface = SolverInterface
 
 include("defaultsolvers.jl")
 
-require(joinpath(dirname(@__FILE__),"HighLevelInterface","HighLevelInterface.jl"))
-using HighLevelInterface
+include(joinpath(dirname(@__FILE__),"HighLevelInterface","HighLevelInterface.jl"))
+using .HighLevelInterface
 export linprog, mixintprog, quadprog
 
 end
