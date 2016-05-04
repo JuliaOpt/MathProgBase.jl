7660bca57b37446d147c6d48097dbb7bf2a67875
Chris C <chriscoey@users.noreply.github.com>
2016-05-03 21:32:55 -0400

    making it run the soc, exp, sdp tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,3 +12,7 @@ quadprogtest()
 # Test conic fallback for LPs
 include("conicinterface.jl")
 coniclineartest(MathProgBase.defaultLPsolver)
+conicSOCtest(MathProgBase.defaultConicsolver)
+conicSOCINTtest(MathProgBase.defaultQPsolver)
+conicEXPtest(MathProgBase.defaultConicsolver)
+conicSDPtest(MathProgBase.defaultSDPsolver)
