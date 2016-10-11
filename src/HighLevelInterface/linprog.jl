397bde1aed3acb38c771e5e459d4d3b5b523a9ae
Laurent Heirendt <laurent.heirendt@uni.lu>
2016-10-11 13:14:43 +0200

    Added documentation and minor style changes

--- src/HighLevelInterface/linprog.jl
+++ src/HighLevelInterface/linprog.jl
@@ -19,83 +19,81 @@ function expandvec(x,len::Integer)
 end
 
 function buildlp(c::InputVector, A::AbstractMatrix, rowlb::InputVector, rowub::InputVector, lb::InputVector, ub::InputVector, solver::AbstractMathProgSolver = MathProgBase.defaultLPsolver)
-  m = LinearQuadraticModel(solver)
-  nrow,ncol = size(A)
+    m = LinearQuadraticModel(solver)
+    nrow,ncol = size(A)
 
-  c = expandvec(c, ncol)
-  rowlbtmp = expandvec(rowlb, nrow)
-  rowubtmp = expandvec(rowub, nrow)
-  lb = expandvec(lb, ncol)
-  ub = expandvec(ub, ncol)
+    c = expandvec(c, ncol)
+    rowlbtmp = expandvec(rowlb, nrow)
+    rowubtmp = expandvec(rowub, nrow)
+    lb = expandvec(lb, ncol)
+    ub = expandvec(ub, ncol)
 
-  # rowlb is allowed to be vector of senses
-  if eltype(rowlbtmp) == Char
-      realtype = eltype(rowubtmp)
-      warn_no_inf(realtype)
-      sense = rowlbtmp
-      rhs = rowubtmp
-      @assert realtype <: Real
+    # rowlb is allowed to be vector of senses
+    if eltype(rowlbtmp) == Char
+        realtype = eltype(rowubtmp)
+        warn_no_inf(realtype)
+        sense = rowlbtmp
+        rhs = rowubtmp
+        @assert realtype <: Real
 
-      rowlb = Array(realtype, nrow)
-      rowub = Array(realtype, nrow)
-      for i in 1:nrow
-          if sense[i] == '<'
-              rowlb[i] = typemin(realtype)
-              rowub[i] = rhs[i]
-          elseif sense[i] == '>'
-              rowlb[i] = rhs[i]
-              rowub[i] = typemax(realtype)
-          elseif sense[i] == '='
-              rowlb[i] = rhs[i]
-              rowub[i] = rhs[i]
-          else
-              error("Unrecognized sense '$(sense[i])'")
-          end
-      end
-  else
-      rowlb = rowlbtmp
-      rowub = rowubtmp
-  end
+        rowlb = Array(realtype, nrow)
+        rowub = Array(realtype, nrow)
+        for i in 1:nrow
+            if sense[i] == '<'
+                rowlb[i] = typemin(realtype)
+                rowub[i] = rhs[i]
+            elseif sense[i] == '>'
+                rowlb[i] = rhs[i]
+                rowub[i] = typemax(realtype)
+            elseif sense[i] == '='
+                rowlb[i] = rhs[i]
+                rowub[i] = rhs[i]
+            else
+                error("Unrecognized sense '$(sense[i])'")
+            end
+        end
+    else
+        rowlb = rowlbtmp
+        rowub = rowubtmp
+    end
 
-  loadproblem!(m, A, lb, ub, c, rowlb, rowub, :Min)
+    loadproblem!(m, A, lb, ub, c, rowlb, rowub, :Min)
 
-  return m
+    return m
 end
 
 function solvelp(m)
-  optimize!(m)
-  stat = status(m)
-  if stat == :Optimal
-      attrs = Dict()
-      attrs[:redcost] = getreducedcosts(m)
-      attrs[:lambda] = getconstrduals(m)
-      return LinprogSolution(stat, getobjval(m), getsolution(m), attrs)
-  elseif stat == :Infeasible
-      attrs = Dict()
-      try
-          attrs[:infeasibilityray] = getinfeasibilityray(m)
-      catch
-          warn("Problem is infeasible, but infeasibility ray (\"Farkas proof\") is unavailable; check that the proper solver options are set.")
-      end
-      return LinprogSolution(stat, nothing, [], attrs)
-  elseif stat == :Unbounded
-      attrs = Dict()
-      try
-          attrs[:unboundedray] = getunboundedray(m)
-      catch
-          warn("Problem is unbounded, but unbounded ray is unavailable; check that the proper solver options are set.")
-      end
-      return LinprogSolution(stat, nothing, [], attrs)
-  else
-      return LinprogSolution(stat, nothing, [], Dict())
-  end
+    optimize!(m)
+    stat = status(m)
+    if stat == :Optimal
+        attrs = Dict()
+        attrs[:redcost] = getreducedcosts(m)
+        attrs[:lambda] = getconstrduals(m)
+        return LinprogSolution(stat, getobjval(m), getsolution(m), attrs)
+    elseif stat == :Infeasible
+        attrs = Dict()
+        try
+            attrs[:infeasibilityray] = getinfeasibilityray(m)
+        catch
+            warn("Problem is infeasible, but infeasibility ray (\"Farkas proof\") is unavailable; check that the proper solver options are set.")
+        end
+        return LinprogSolution(stat, nothing, [], attrs)
+    elseif stat == :Unbounded
+        attrs = Dict()
+        try
+            attrs[:unboundedray] = getunboundedray(m)
+        catch
+            warn("Problem is unbounded, but unbounded ray is unavailable; check that the proper solver options are set.")
+        end
+        return LinprogSolution(stat, nothing, [], attrs)
+    else
+        return LinprogSolution(stat, nothing, [], Dict())
+    end
 end
 
 function linprog(c::InputVector, A::AbstractMatrix, rowlb::InputVector, rowub::InputVector, lb::InputVector, ub::InputVector, solver::AbstractMathProgSolver = MathProgBase.defaultLPsolver)
-
-  m = buildlp(c, A, rowlb, rowub, lb, ub, solver)
-  return solvelp(m)
-
+    m = buildlp(c, A, rowlb, rowub, lb, ub, solver)
+    return solvelp(m)
 end
 
 linprog(c,A,rowlb,rowub, solver::AbstractMathProgSolver = MathProgBase.defaultLPsolver) = linprog(c,A,rowlb,rowub,0,Inf, solver)
