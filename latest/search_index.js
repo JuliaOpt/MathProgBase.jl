dafebf6b39d45c56fc7dcfa33dd55c46695feca7
autodocs <autodocs>
2017-06-07 22:08:32 +0000

    build based on 16b0a18

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "MathProgBase documentation",
+    "title": "MathProgBase documentation",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#MathProgBase-documentation-1",
+    "page": "MathProgBase documentation",
+    "title": "MathProgBase documentation",
+    "category": "section",
+    "text": ""
+},
+
+]}
