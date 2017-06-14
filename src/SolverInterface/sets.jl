8f0662b2fb6de6787442f81cf710e5a7db1d46a8
Miles Lubin <miles.lubin@gmail.com>
2017-06-14 14:14:31 -0400

    argument naming

--- src/SolverInterface/sets.jl
+++ src/SolverInterface/sets.jl
@@ -50,16 +50,16 @@ end
 dimension(s::Interval) = 1
 
 """
-    addconstraint!(m::AbstractMathProgModel, b, a_varidx, a_coef, Q_vari, Q_varj, Q_val, S::AbstractSet)
+    addconstraint!(m::AbstractMathProgModel, b, a_varidx, a_coef, Q_vari, Q_varj, Q_coef, S::AbstractSet)
 
 Add the constraint
 ```math
 a^Tx + b + \\frac{1}{2}x^TQx \\in S
 ```
 where ``a`` is a sparse vector specified in tuple form by
-`a_varidx`, and `a_val`; ``b`` is a scalar;
+`a_varidx`, and `a_coef`; ``b`` is a scalar;
 the symmetric matrix ``Q_i`` is defined by the triplets in `Q_vari`, `Q_varj`,
-`Q_val`; and the set ``S`` is defined by `S`.
+`Q_coef`; and the set ``S`` is defined by `S`.
 
 Duplicate indices in either `a_varidx` or `Q_vari` and `Q_varj` are accepted and will be summed together. Off-diagonal entries of ``Q`` will be mirrored, so either the upper triangular or lower triangular entries of ``Q`` should be provided. If entries for both ``(i,j)`` and ``(j,i)`` are provided, these are considered duplicate terms.
 """
