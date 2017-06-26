5a7e00ff481f9e7be7355e0ce589a464efefff08
Chris Coey <coey.chris@gmail.com>
2017-06-26 18:15:17 -0400

    added exp and pow sets, tidied definitions

--- src/SolverInterface/sets.jl
+++ src/SolverInterface/sets.jl
@@ -5,41 +5,50 @@ Abstract supertype for set objects used to encode constraints.
 """
 abstract type AbstractSet end
 
+dimension(s::AbstractSet) = s.dim
 
 """
-    NonNegative(n)
+    Free(dim)
 
-The nonnegative orthant ``\\{ x \\in \\mathbb{R}^n : x \\ge 0 \\}`` where the dimension ``n`` is specified by the field `n`.
+The set ``\\mathbb{R}^{dim}`` (containing all points) of dimension ``dim``.
+"""
+struct Zero <: AbstractSet
+    dim::Int
+end
+
+"""
+    Zero(dim)
+
+The set ``\\{ 0 \\}^{dim}`` (containing only the origin) of dimension ``dim``.
+"""
+struct Zero <: AbstractSet
+    dim::Int
+end
+
+"""
+    NonNegative(dim)
+
+The nonnegative orthant ``\\{ x \\in \\mathbb{R}^{dim} : x \\ge 0 \\}`` of dimension ``dim``.
 """
 struct NonNegative <: AbstractSet
     dim::Int
 end
 
 """
-    NonPositive(n)
+    NonPositive(dim)
 
-The nonpositive orthant ``\\{ x \\in \\mathbb{R}^n : x \\le 0 \\}`` where the dimension ``n`` is specified by the field `n`.
+The nonpositive orthant ``\\{ x \\in \\mathbb{R}^{dim} : x \\le 0 \\}`` of dimension ``dim``.
 """
 struct NonPositive <: AbstractSet
     dim::Int
 end
 
 """
-    Zero(n)
-
-The set ``\\{0\\}^n`` where the dimension ``n`` is specified by the field `n`.
-"""
-struct Zero <: AbstractSet
-    dim::Int
-end
-
-
-"""
     Interval(lower,upper)
 
-The set ``[l,u] \\subseteq \\mathbb{R}^n`` where ``l`` and ``u`` are specified by `lower` and `upper`, respectively. We allow `lower` and `upper` to be `-Inf` or `Inf`, in which case the set is interpreted as a one-sided interval.
+The box ``[lower, upper] \\subseteq \\mathbb{R}^{dim}`` where ``lower`` and ``upper`` are vectors of dimension ``dim``. If ``lower`` or ``upper`` is all ``-Inf`` or ``Inf``, the set is interpreted as a one-sided interval.
 """
-struct Interval{T} <: AbstractSet
+struct Interval{T <: Real} <: AbstractSet
     lower::T
     upper::T
 end
@@ -47,27 +56,54 @@ end
 dimension(s::Interval) = length(s.lower)
 
 """
-    SecondOrderCone(n)
+    SecondOrderCone(dim)
 
-The second-order cone or the Lorenz cone of dimension `n`
-defined as
-```math
-\\{ (t,x) \\in \\mathbb{R}^n : t \\ge ||x||_2 \\}.
-```
+The second-order cone (or Lorenz cone) ``\\{ (t,x) \\in \\mathbb{R}^{dim} : t \\ge || x ||_2 \\}`` of dimension ``dim``.
 """
 struct SecondOrderCone <: AbstractSet
     dim::Int
 end
 
-#ExponentialCone
+"""
+    ExponentialCone()
+
+The 3-dimensional exponential cone ``\\{ (x,y,z) \\in \\mathbb{R}^3 : y \\exp{x/y} \\le z, y > 0 \\}``.
+"""
+struct ExponentialCone <: AbstractSet
+end
 
 """
-    PositiveSemidefiniteConeTriangle(n)
+    DualExponentialCone()
 
-The cone of symmetric ``n \\times n`` matrices that are positive semidefinite.
-The dimension of the cone is ``n(n+1)/2`` since the matrices are symmetric.
-The entries of the upper triangular part of the matrix are given row by row (or equivalently, the entries of the lower triangular part are given column by column).
-The scalar product is the sum of the pairwise product of the diagonal entries plus twice the sum of the pairwise product of the upper diagonal entries.
+The 3-dimensional dual exponential cone ``\\{ (u,v,w) \\in \\mathbb{R}^3 : -u \\exp{v/u} \\le \\exp{1} w, u < 0 \\}``.
+"""
+struct DualExponentialCone <: AbstractSet
+end
+
+"""
+    PowerCone(a)
+
+The 3-dimensional power cone ``\\{ (x,y,z) \\in \\mathbb{R}^3 : x^{a} y^{1-a} >= |z|, x \\ge 0, y \\ge 0 \\}`` with parameter ``a``.
+"""
+struct PowerCone{T <: Real} <: AbstractSet
+    a::T
+end
+
+"""
+    DualPowerCone(a)
+
+The 3-dimensional power cone ``\\{ (u,v,w) \\in \\mathbb{R}^3 : (u/a)^a (v/(1-a))^{1-a} >= |w|, u \\ge 0, v \\ge 0 \\}`` with parameter ``a``.
+"""
+struct DualPowerCone{T <: Real} <: AbstractSet
+    a::T
+end
+
+dimension(s::Union{ExponentialCone, DualExponentialCone, PowerCone, DualPowerCone}) = 3
+
+"""
+    PositiveSemidefiniteConeTriangle(dim)
+
+The cone of symmetric ``dim \\times dim`` matrices that are positive semidefinite. The dimension of the cone is ``dim (dim + 1)/2`` since the matrices are symmetric. The entries of the upper triangular part of the matrix are given row by row (or equivalently, the entries of the lower triangular part are given column by column). The scalar product is the sum of the pairwise product of the diagonal entries plus twice the sum of the pairwise product of the upper diagonal entries.
 
 ### Examples
 
@@ -83,15 +119,13 @@ corresponds to ``(1, 2, 3, 4, 5, 6)`` for `PositiveSemidefiniteConeTriangle`
 """
 struct PositiveSemidefiniteConeTriangle <: AbstractSet
     dim::Int
+    sidedim::Int
 end
 
 """
     PositiveSemidefiniteConeScaled(n)
 
-The cone of symmetric ``n \\times n`` matrices that are positive semidefinite.
-The dimension of the cone is ``n(n+1)/2`` since the matrices are symmetric.
-The entries of the upper triangular part of the matrix are given row by row (or equivalently, the entries of the lower triangular part are given column by column).
-The off-diagonal entries of the matrices of both the cone and its dual are scaled by ``\\sqrt{2}`` and the scalar product is simply the sum of the pairwise product of the entries.
+The cone of symmetric ``dim \\times dim`` matrices that are positive semidefinite. The dimension of the cone is ``dim (dim + 1)/2`` since the matrices are symmetric. The entries of the upper triangular part of the matrix are given row by row (or equivalently, the entries of the lower triangular part are given column by column). The off-diagonal entries of the matrices of both the cone and its dual are scaled by ``\\sqrt{2}`` and the scalar product is simply the sum of the pairwise product of the entries.
 
 ### Examples
 
@@ -107,51 +141,43 @@ and to ``(1, 2\\sqrt{2}, 3\\sqrt{2}, 4, 5\\sqrt{2}, 6)`` for `PositiveSemidefini
 """
 struct PositiveSemidefiniteConeScaled <: AbstractSet
     dim::Int
+    sidedim::Int
 end
 
-dimension(s::Union{PositiveSemidefiniteConeScaled, PositiveSemidefiniteConeTriangle}) = (s.dim * (s.dim + 1)) / 2
-
 """
-    Integers(n)
+    Integers(dim)
 
-The set of integers ``\\mathbb{Z}^n``.
+The set of integers ``\\mathbb{Z}^{dim}``.
 """
 struct Integers <: AbstractSet
     dim::Int
 end
 
 """
-    Binaries(n)
+    Binaries(dim)
 
-The set of binary vectors ``\\{0,1\\}^n``.
+The set of binary vectors ``\\{ 0, 1 \\}^{dim}``.
 """
 struct Binaries <: AbstractSet
     dim::Int
 end
 
-
-dimension(s::Union{NonNegative,NonPositive,Zero,SecondOrderCone,Integers,Binaries}) = s.dim
-
 """
-    SOS1(weights::Vector{T}) where T
+    SOS1(weights)
 
-The set corresponding to the special ordered set (SOS) constraint of type 1. Of the variables in the set, at most one can be nonzero. The ``weights`` induce an ordering of the variables; as such, they should be unique values. The ``k``-th element in the set corresponds to the ``k``-th weight in ``weights``.
-
-See [here](http://lpsolve.sourceforge.net/5.5/SOS.htm) for a description of SOS constraints and their potential uses.
+The set corresponding to the special ordered set (SOS) constraint of type 1. Of the variables in the set, at most one can be nonzero. The ``weights`` induce an ordering of the variables; as such, they should be unique values. The ``k``-th element in the set corresponds to the ``k``-th weight in ``weights``. See [here](http://lpsolve.sourceforge.net/5.5/SOS.htm) for a description of SOS constraints and their potential uses.
 """
-struct SOS1{T} <: AbstractSet
+struct SOS1{T <: Real} <: AbstractSet
     weights::Vector{T}
 end
-dimension(s::SOS1) = length(s.weights)
 
 """
-    SOS2(weights::Vector{T}) where T
+    SOS2(weights)
 
-The set corresponding to the special ordered set (SOS) constraint of type 2. Of the variables in the set, at most two can be nonzero, and if two are nonzero, they must be adjacent in the ordering of the set. The ``weights`` induce an ordering of the variables; as such, they should be unique values. The ``k``-th element in the set corresponds to the ``k``-th weight in ``weights``.
-
-See [here](http://lpsolve.sourceforge.net/5.5/SOS.htm) for a description of SOS constraints and their potential uses.
+The set corresponding to the special ordered set (SOS) constraint of type 2. Of the variables in the set, at most two can be nonzero, and if two are nonzero, they must be adjacent in the ordering of the set. The ``weights`` induce an ordering of the variables; as such, they should be unique values. The ``k``-th element in the set corresponds to the ``k``-th weight in ``weights``. See [here](http://lpsolve.sourceforge.net/5.5/SOS.htm) for a description of SOS constraints and their potential uses.
 """
-struct SOS2{T} <: AbstractSet
+struct SOS2{T <: Real} <: AbstractSet
     weights::Vector{T}
 end
-dimension(s::SOS2) = length(s.weights)
+
+dimension(s::Union{SOS1, SOS2}) = length(s.weights)
