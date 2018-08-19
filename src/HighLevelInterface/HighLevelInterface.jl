3ff98f8c1468576b78cbd560791074e70b419557
Miles Lubin <mlubin@google.com>
2018-08-18 23:10:45 -0400

    remove deprecated use of warn_once (#217)
    * remove deprecated use of warn_once
    
    * update travis to test on 0.7

--- src/HighLevelInterface/HighLevelInterface.jl
+++ src/HighLevelInterface/HighLevelInterface.jl
@@ -6,7 +6,7 @@ using Compat
 
 function warn_no_inf(T)
     if !(isinf(typemin(T)) && isinf(typemax(T)))
-        Base.warn_once("Element type $T does not have an infinite value. Note that this may artifically introduce ranged (two-sided) constraints. To avoid this, consider casting the problem data to Float64.")
+        @Compat.warn("Element type $T does not have an infinite value. Note that this may artifically introduce ranged (two-sided) constraints. To avoid this, consider casting the problem data to Float64.")
     end
 end
 
